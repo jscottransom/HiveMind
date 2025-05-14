5de615e40bd3c59a995e9ffc493fa9e5ac191ac4
jscottransom <james.scottran@gmail.com>
2025-05-14 14:45:55 -0700

    feat: Add simple http server

--- interface/cmd/main.go
+++ interface/cmd/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"io"
 )
 
 func main() {
-	fmt.Println("Welcome to the HiveMind Interface")
+	http.HandleFunc("/", handleRoot)
+	fmt.Println("HiveMind Server running on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		startupError := fmt.Errorf("Error starting up on Port 8080")
+		fmt.Println(startupError)
+	}
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Welcome to HiveMind\n")
 }
